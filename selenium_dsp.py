d700b319bfa207be79c91c6d51d5dbcd4cb864eb
Dilshad Adam <dilshaat@gmail.com>
2018-04-04 11:46:52 +1000

    testing chromedrive

--- selenium_dsp.py
+++ selenium_dsp.py
@@ -1,2 +1,11 @@
-import sys
-print(sys.version)
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome(r'C:\WebDrivers\chromedriver.exe')
+driver.get('https://www.google.com')
+time.sleep(5)
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromDriver')
+search_box.submit()
+time.sleep(5)
+driver.quit()
